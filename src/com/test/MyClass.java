91a6f5f574910b208e8e32bcedae4f61818feb6d
prajith1234 <challapkumar07@gmail.com>
2020-11-13 14:45:42 +0530

    addnew

--- src/com/test/MyClass.java
+++ src/com/test/MyClass.java
@@ -5,6 +5,9 @@ public class MyClass {
 	public void myclass() {
 		System.out.println("Hello MyClass");
 	}
+	public void myclass1() {
+		System.out.println("Hello MyClass1");
+	}
 
 	public static void main(String[] args) {
 
